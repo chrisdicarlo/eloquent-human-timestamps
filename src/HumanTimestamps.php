bca5faa7d1abe60baf5862224112d9e1fa2affa0
Chris Di Carlo <chris@dicarlosystems.ca>
2021-04-18 14:31:10 -0400

    RChange function call to Str::before to allow support on Laravel 6.x

--- src/HumanTimestamps.php
+++ src/HumanTimestamps.php
@@ -9,9 +9,9 @@ trait HumanTimestamps
     public function __get($key)
     {
         if(Str::endsWith($key, '_for_humans') &&
-            $this->getAttribute(Str::beforeLast($key, '_for_humans')) &&
-            $this->isDateAttribute(Str::beforeLast($key, '_for_humans'))) {
-                return $this->getAttribute(Str::beforeLast($key, '_for_humans'))->diffForHumans();
+            $this->getAttribute(Str::before($key, '_for_humans')) &&
+            $this->isDateAttribute(Str::before($key, '_for_humans'))) {
+                return $this->getAttribute(Str::before($key, '_for_humans'))->diffForHumans();
             }
 
         return parent::__get($key);
